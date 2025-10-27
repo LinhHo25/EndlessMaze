2481fe09dff567c18a743d59e0e924e265a19c0f
Huy <ngohuy171105@gmail.com>
2025-10-27 21:46:04 +0700

    BoSungLan2

--- Main/BLL_HUY/Player.cs
+++ Main/BLL_HUY/Player.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Main.BLL_HUY
 {
-    internal class Player
+    public class Player : Character
     {
+        public Inventory Inventory { get; private set; }
+
+        public Player(int health, int attack, Vector2 position)
+            : base(health, attack, position)
+        {
+            Inventory = new Inventory();
+        }
+
+        protected override void Die()
+        {
+            Console.WriteLine("Người chơi đã hy sinh! Game Over.");
+        }
+
+        public bool CheckForItem(string itemId)
+        {
+            return Inventory.ContainsItem(itemId);
+        }
+
+        public void Move(Vector2 direction)
+        {
+            Position = Position + direction * 0.5f;
+        }
     }
 }

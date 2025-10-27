4a77d47127886cdde03574b8516c31ffb808ac9b
Huy <ngohuy171105@gmail.com>
2025-10-27 21:53:06 +0700

    BOsung

--- Main/BLL_HUY/Inventory.cs
+++ Main/BLL_HUY/Inventory.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Main.BLL_HUY
 {
-    internal class Inventory
+    public class Inventory
     {
+        private List<IItem> items = new List<IItem>();
+        private Dictionary<ItemSlot, EquipmentItem> equippedItems = new Dictionary<ItemSlot, EquipmentItem>();
+
+        public void AddItem(IItem item)
+        {
+            items.Add(item);
+            Console.WriteLine($"-> Nhặt: {item.Name}.");
+        }
+
+        public bool ContainsItem(string itemId)
+        {
+            return items.Any(i => i.ItemId == itemId);
+        }
+
+        public void Equip(EquipmentItem item)
+        {
+            equippedItems[item.Slot] = item;
+            Console.WriteLine($"-> Trang bị {item.Name} thành công.");
+        }
     }
 }
